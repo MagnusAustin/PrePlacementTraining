e83b53ed06bbd5d368e0a0a0f7092abe29b29873
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-15 12:17:13 +0530

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,18 @@
+class Test
+{
+    static public void show()
+    {
+        System.out.println("Hi");
+    }
+}
+public class Solution1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Test.show();
+    }
+    
+}
