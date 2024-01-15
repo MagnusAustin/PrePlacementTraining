06a906fe3059406cdfbe777df3ac9ca568991d57
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-15 12:18:19 +0530

    Create Solution3.java

---
+++ Solution3.java
@@ -0,0 +1,12 @@
+public class Solution3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=0;
+        System.out.println(i);
+    }
+    
+}
