7d2520070e41740748825a59ddd9e998e2b631e3
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-16 11:12:31 +0530

    Create Palindrome.java

---
+++ Day2/Palindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author anike
+ */
+public class Palindrome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter Number");
+        int a = sc.nextInt();
+        int i =0;
+        int sum=0;
+        int temp =a;
+
+        while(a!=0){
+            sum=a%10;
+            i=i*10+sum;
+            a=a/10;
+        }
+        
+        if(temp==i)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not Palindrome");
+ 
+    }
+}
