4f61d03c07aa87a403b41746abd9a6931a10a6a7
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-18 15:41:44 +0530

    Create StatPref.java

---
+++ Day4/StatPref.java
@@ -0,0 +1,17 @@
+public class JavaApplication5 {
+    /**
+     * @param args the command line arguments
+     */
+    static{
+        System.out.println("I am in static block 1");
+    }     
+    static{
+        System.out.println("I am in static block 2");
+    }       
+    public static void main(String[] args) {
+        System.out.println("I am in main");
+    }      
+    static{
+        System.out.println("I am in static block 3");
+    }
+}
