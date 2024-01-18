0fd71a08bfba4b39252af218131bd44c60268ccd
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-18 09:23:49 +0530

    Create MissingValue.java

---
+++ Day3/MissingValue.java
@@ -0,0 +1,15 @@
+public class JavaApplication5 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int arr[] = {13, 12, 11, 15};
+        Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] != 1) {
+                System.out.println("Missing value is: " + (arr[i] + 1));
+            }
+        }
+    }    
+}
