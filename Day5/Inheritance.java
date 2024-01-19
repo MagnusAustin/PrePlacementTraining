66eca328da3cd07d6da05138ad166fb3c8cc8152
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-19 10:26:12 +0530

    Update Inheritance.java

--- Day5/Inheritance.java
+++ Day5/Inheritance.java
@@ -1,1 +1,37 @@
+class A{
+    void showA(){
+        System.out.println("I am in A");
+    }
+}
 
+class B extends A{
+    void showB(){
+        System.out.println("I am in B");
+    }
+}
+
+class C extends B{
+    void showC(){
+        System.out.println("I am in C");
+    }
+}
+
+class D extends A{
+    void showD(){
+        System.out.println("I am in D");
+    }
+}
+public class JavaApplication5 {
+    /**
+     * @param args the command line arguments
+     */     
+    public static void main(String[] args) {
+        C c = new C();
+        c.showA();
+        c.showB();
+        c.showC();
+        D d = new D();
+        d.showA();
+        d.showD();
+    }      
+}
