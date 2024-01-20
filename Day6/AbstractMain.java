6847f9db19534d2c31d92d4eae465062a25da105
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-20 11:39:51 +0530

    Create AbstractMain.java

---
+++ Day6/AbstractMain.java
@@ -0,0 +1,9 @@
+abstract class Test {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Main method");   
+    }
+}
