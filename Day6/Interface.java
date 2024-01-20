bd4c07a139f50c63e590d53330fbd77ec610d043
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-01-20 10:46:54 +0530

    Update Interface.java

--- Day6/Interface.java
+++ Day6/Interface.java
@@ -1,1 +1,24 @@
+interface ABC{
+    public void show();
+}
 
+class PQR{
+    static int i= 20;
+}   
+public class JavaApplication5 extends PQR implements ABC {
+    /**
+     * @param args the command line arguments
+     */
+    public void show(){
+        System.out.println("I am in ABC");
+    }
+    
+    public static void main(String[] args) {
+        ABC a;
+        JavaApplication5 z = new JavaApplication5();
+        a=z;
+        a.show();
+        System.out.println(i);
+    }
+         
+}
